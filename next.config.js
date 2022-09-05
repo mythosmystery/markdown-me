9f3cfe98527a17823ce3bf7f8f688a20ee3a4257
Hunter Barton <axiysfire@gmail.com>
2022-09-05 02:23:51 -0500

    feat: added docker

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
-const isProd = process.env.NODE_ENV === 'production';
-const withPWA = require('next-pwa');
-const { runtimeCaching } = require('next-pwa/cache');
+// const isProd = process.env.NODE_ENV === 'production';
+// const withPWA = require('next-pwa');
+// const { runtimeCaching } = require('next-pwa/cache');
 
-/**
- * @type {import('next').NextConfig}
- **/
+// /**
+//  * @type {import('next').NextConfig}
+//  **/
 
-module.exports = withPWA({
-   images: {
-      loader: 'imgix',
-      path: isProd ? 'https://markdownit.ga' : 'http://localhost:3000'
-   },
-   pwa: {
-      dest: 'public',
-      runtimeCaching,
-      buildExcludes: [/middleware-manifest.json$/],
-      register: true,
-      skipWaiting: true
-   }
-});
+// module.exports = withPWA({
+//    images: {
+//       loader: 'imgix',
+//       path: isProd ? 'https://markdownit.ga' : 'http://localhost:3000'
+//    },
+//    pwa: {
+//       dest: 'public',
+//       runtimeCaching,
+//       buildExcludes: [/middleware-manifest.json$/],
+//       register: true,
+//       skipWaiting: true
+//    }
+// });
